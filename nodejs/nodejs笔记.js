8a1baa244bae05f6f7755686c8978c674ee70b90
LingYanSi <1029891106@qq.com>
2015-04-16 17:57:29 +0800

    关于this.call.apply.作用域等等

--- nodejs/nodejs笔记.js
+++ nodejs/nodejs笔记.js
@@ -145,3 +145,15 @@
 				req.end(); 
 			});
 
+-------------------------------------------------------------------------------------------------------------
+	npm install xxx 安装模块
+
+	npm install xxx -g 将模块安装到全局环境中 参考http://goddyzhao.tumblr.com/post/9835631010/no-direct-command-for-local-installed-command-line-modul
+
+	npm ls 查看安装的模块及依赖
+
+	npm ls -g 查看全局安装的模块及依赖
+
+	npm uninstall xxx  (-g) 卸载模块
+
+	npm cache clean 清理缓存
