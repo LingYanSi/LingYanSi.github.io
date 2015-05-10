b2ce0a5e7d1328d17b73fbd1efc6600cd0fe8543
LingYanSi <1029891106@qq.com>
2015-05-10 23:08:38 +0800

    新增了mouse以及key事件

--- demo/js/swipe-new1.js
+++ demo/js/swipe-new1.js
@@ -43,7 +43,7 @@
 				leftMax = idWidth ,
 				leftCurrent = 0 ;
 			}
-			$item.not(':eq('+currentPage+')').css({webkitTransform:'translate('+leftMax+'px,'+topMax+'px)'}); //设置元素初始位置
+			$item.not(':eq('+currentPage+')').css({webkitTransform:'translate3d('+leftMax+'px,'+topMax+'px,0)'}); //设置元素初始位置
 			if (dianNav)
 			{
 				appendDian();
@@ -52,6 +52,11 @@
 			}
 			if (autoPlay) start();
 			touchMove(idname);//touch事件
+			window.addEventListener('keyup',function(event){ // 按键监听
+				var index ;
+				if (event.keyCode==40 || event.keyCode==39) index = setPage((currentPage-1) ),toWhere(index,'next');
+				else if (event.keyCode==38 || event.keyCode==37)  index = setPage((currentPage+1) ),toWhere(index,'prev');
+			});
 			function touchMove(idname){ //触摸事件
 				var id = document.getElementById(idname);
 				var xx,XX,$current,currentDom,$prev,prevDom,$next,nextDom,swipeX,swipeY,cha;
@@ -80,7 +85,6 @@
 				id.addEventListener('touchmove',function(event){
 					XX = event.targetTouches[0].screenX;
 					YY = event.targetTouches[0].screenY;
-					//document.querySelector('#log1').innerHTML = new Date().getTime()
 					
 					if (swipeY && (!swipeX || Math.abs(XX-xx)-Math.abs(YY-yy)<0))
 					{
@@ -125,13 +129,14 @@
 							}
 						}
 					}
-					//document.querySelector('#log1').innerHTML = new Date().getTime()
 				});
 				id.addEventListener('touchend',function(event){
-					//document.querySelector('#log1').innerHTML = new Date().getTime()
-					currentDom.classList.add('change');
-					prevDom.classList.add('change');
-					nextDom.classList.add('change');
+					if(cha != 0)
+					{
+						currentDom.classList.add('change');
+						prevDom.classList.add('change');
+						nextDom.classList.add('change');
+					}
 					if (!toLeft && swipeY )
 					{
 						if (cha>0)
@@ -189,7 +194,154 @@
 					}
 					if (autoPlay) start();
 					if(dianNav) dianMove();
-					//document.querySelector('#log2').innerHTML = new Date().getTime()
+					if (callback)
+					{
+						setTimeout(function(){
+							callback(currentPage);
+						},600)
+					}
+				});
+			}
+			mouseEvent(idname); //鼠标滑动事件
+			function mouseEvent(idname){
+				var id = document.getElementById(idname);
+				var xx,XX,$current,currentDom,$prev,prevDom,$next,nextDom,swipeX,swipeY,cha;
+				id.addEventListener('mousedown',function(event){
+					stop();
+					xx = event.pageX;
+					yy = event.pageY;
+					XX = xx ;
+					YY = yy ;
+					cha = 0 ;
+					$prev = $item.eq(currentPage-1)[0]?$item.eq(currentPage-1):$item.eq(len-1);
+					$next = $item.eq(currentPage+1)[0]?$item.eq(currentPage+1):$item.eq(0);
+					$current = $item.eq(currentPage);
+
+					currentDom = $current[0],prevDom=$prev[0],nextDom=$next[0];
+					
+					currentDom.classList.remove('change');
+					prevDom.classList.remove('change');
+					nextDom.classList.remove('change');
+
+					prevDom.style.webkitTransform = 'translate3d('+leftMin+'px,'+topMax+'px,0)' ;
+					nextDom.style.webkitTransform = 'translate3d('+leftMin+'px,'+topMax+'px,0)' ;
+					swipeY = true ;
+					swipeX = true ;
+				});
+				id.addEventListener('mousemove',function(event){
+					XX = event.pageX;
+					YY = event.pageY;
+					
+					if (swipeY && (!swipeX || Math.abs(XX-xx)-Math.abs(YY-yy)<0))
+					{
+						swipeX = false ;
+						if (!toLeft)
+						{
+							event.stopPropagation();
+							event.preventDefault();
+							cha = YY-yy ;
+							if (cha>=0)
+							{
+								if (!loop && currentPage==0) return swipeY=false; // 循环模块
+								currentDom.style.webkitTransform = 'translate3d(0,'+(cha)+'px,0)';
+								prevDom.style.webkitTransform = 'translate3d(0,0,0)' ;
+								if (len>2) nextDom.style.webkitTransform = 'translate3d(0,'+(topMax)+'px,0)' ;// 避免因为滑动过快引起的bug
+							}else{
+								if (!loop && currentPage==len-1) return swipeY=false; // 循环模块
+								currentDom.style.webkitTransform = 'translate3d(0,0,0)';
+								nextDom.style.webkitTransform = 'translate3d(0,'+(topMax+cha)+'px,0)' ;
+								if (len>2) prevDom.style.webkitTransform = 'translate3d(0,'+(topMax)+'px,0)' ;// 避免因为滑动过快引起的bug
+							}
+						}
+					}
+					if(swipeX && (!swipeY || Math.abs(XX-xx)-Math.abs(YY-yy)>=0) ){
+						swipeY = false ;
+						if (toLeft)
+						{
+							event.stopPropagation();
+							event.preventDefault();
+							cha = XX-xx ;
+							if (cha>=0)
+							{
+								if (!loop && currentPage==0) return swipeX=false; // 循环模块
+								currentDom.style.webkitTransform = 'translate3d('+( XX-xx)+'px,0,0)';
+								prevDom.style.webkitTransform = 'translate3d('+(leftMin+cha)+'px,0,0)';
+								if (len>2) nextDom.style.webkitTransform = 'translate3d('+leftMax+'px,0,0)';// 避免因为滑动过快引起的bug
+							}else{
+								if (!loop && currentPage==len-1) return swipeX=false; // 循环模块
+								currentDom.style.webkitTransform = 'translate3d('+cha+'px,0,0)';
+								nextDom.style.webkitTransform = 'translate3d('+(leftMax+cha)+'px,0,0)';
+								if (len>2) prevDom.style.webkitTransform = 'translate3d('+leftMax+'px,0,0)';// 避免因为滑动过快引起的bug
+							}
+						}
+					}
+				});
+				document.addEventListener('mouseup',function(event){
+					if(cha != 0)
+					{
+						currentDom.classList.add('change');
+						prevDom.classList.add('change');
+						nextDom.classList.add('change');
+					}
+					if (!toLeft && swipeY )
+					{
+						if (cha>0)
+						{
+							if (cha>50)
+							{
+								currentDom.style.webkitTransform = 'translate3d(0,'+(topMax)+'px,0)';
+								prevDom.style.webkitTransform = 'translate3d(0,0,0)';
+
+								currentPage--;
+								currentPage = setPage(currentPage);
+							}else{
+								currentDom.style.webkitTransform = 'translate3d(0,0,0)';
+								prevDom.style.webkitTransform = 'translate3d(0,0,0)';
+							}
+						}else{
+							if (cha<-50)
+							{
+								currentDom.style.webkitTransform = 'translate3d(0,0,0)';
+								nextDom.style.webkitTransform = 'translate3d(0,0,0)';
+								currentPage++;
+								currentPage = setPage(currentPage);
+							}else{
+								currentDom.style.webkitTransform = 'translate3d(0,0,0)';
+								nextDom.style.webkitTransform = 'translate3d(0,'+(topMax)+'px,0)';
+							}
+						}
+					}
+					else if ( toLeft && swipeX)
+					{
+						if (cha>0)
+						{
+							if (cha>50)
+							{
+								currentDom.style.webkitTransform = 'translate3d('+leftMax+'px,0,0)';
+								prevDom.style.webkitTransform = 'translate3d(0,0,0)';
+								currentPage--;
+								currentPage = setPage(currentPage);
+							}else{
+								currentDom.style.webkitTransform = 'translate3d(0,0,0)';
+								prevDom.style.webkitTransform = 'translate3d('+leftMin+'px,0,0)';
+							}
+						}else{
+							if (cha<-50)
+							{
+								currentDom.style.webkitTransform = 'translate3d('+leftMin+'px,0,0)';
+								nextDom.style.webkitTransform = 'translate3d(0,0,0)';
+								currentPage++;
+								currentPage = setPage(currentPage);
+							}else{
+								currentDom.style.webkitTransform = 'translate3d(0,0,0)';
+								nextDom.style.webkitTransform = 'translate3d('+leftMax+'px,0,0)';
+							}
+						}
+					}
+					swipeY = false ;
+					swipeX = false ;
+					if (autoPlay) start();
+					if(dianNav) dianMove();
 					if (callback)
 					{
 						setTimeout(function(){
@@ -199,30 +351,25 @@
 				});
 			}
 			function setPage(page){ //设置page
-				if (page>len-1)
-				{
-					page=0;
-				}else if (page<0)
-				{
-					page=len-1;
-				}
+				if (page>len-1) page=0;
+				else if (page<0) page=len-1;
 				return page ;
 			}
-			function toWhere(index){ //主要是给左右点击事件使用
+			function toWhere(index,dir){ //主要是给左右点击事件使用
 				if (index === currentPage) return
 				stop();
 				$item.removeClass('change');
-				if ((index>currentPage)) //下一张
+				if (dir=='next') //下一张
 				{
-					$item.eq(index).css({webkitTransform:'translate('+leftMax+'px,'+topMax+'px)'});
+					$item.eq(index).css({webkitTransform:'translate3d('+leftMax+'px,'+topMax+'px,0)'});
 					$item.show().addClass('change');
-					$item.eq(currentPage).css({webkitTransform:'translate('+leftMin+'px,'+topMin+'px)'});
-				}else{                                                 // 上一张
-					$item.eq(index).css({webkitTransform:'translate('+leftMin+'px,'+topMin+'px)'});
+					$item.eq(currentPage).css({webkitTransform:'translate3d('+leftMin+'px,'+topMin+'px,0)'});
+				}else if(dir=='prev'){   // 上一张
+					$item.eq(index).css({webkitTransform:'translate3d('+leftMin+'px,'+topMin+'px,0)'});
 					$item.show().addClass('change');
-					$item.eq(currentPage).css({webkitTransform:'translate('+leftMax+'px,'+topMax+'px)'});
+					$item.eq(currentPage).css({webkitTransform:'translate3d('+leftMax+'px,'+topMax+'px,0)'});
 				}
-				$item.eq(index).css({webkitTransform:'translate(0,0)'});
+				$item.eq(index).css({webkitTransform:'translate3d(0,0,0)'});
 				currentPage = index ;
 				dianMove();
 			}
@@ -230,7 +377,6 @@
 			if (buttPrev)
 			{
 				$('#'+buttPrev).click(function(){
-					console.log(currentPage);
 					var index = currentPage-1 ;
 					index = setPage(index);
 					toWhere(index);
