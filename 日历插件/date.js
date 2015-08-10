c45f793a4945a7c3783cfbbd5a776b4a7027990a
LingYanSi <1029891106@qq.com>
2015-08-10 18:06:05 +0800

    初步完成

--- 日历插件/date.js
+++ 日历插件/date.js
@@ -23,7 +23,31 @@
 }*/
 
 // 获取任意两天之间相差的天数
-console.log( bitch('1900-07-1') );
+
+$('#render').on('click',function(){
+	render(this.previousElementSibling.value,{arr:[1,2,3],className:'nidaye'});
+});
+
+function render(str){
+	var df = bitch(str);
+	var $td = $('td');
+	var arg = [].slice.call(arguments).slice(1) ;
+
+
+	$td.elements.forEach(function(ele,index){
+		ele.textContent = '' ;
+		var currentDate = index - df.monthFirstDay+2 ;
+		if(currentDate>0 && currentDate<=df.monthDates ){
+			ele.textContent = currentDate +'' ;
+			arg.forEach(function(ele1){
+				ele1.arr.forEach(function(ele2){
+					if(ele2==currentDate) ele.classList.add(ele1.className);
+				})
+			});
+		}
+	});
+}
+
 function bitch(str){
 	var pointDayArr = str.split('-').map(function(ele){
 		return parseInt(ele,10);
@@ -31,7 +55,7 @@ function bitch(str){
 	var pointDayObj = {
 		year: pointDayArr[0] ,
 		month: pointDayArr[1] ,
-		date : pointDayArr[2] ,
+		date : pointDayArr[2]
 	}
 
 	var date = new Date();
@@ -41,10 +65,51 @@ function bitch(str){
 		date:date.getDate(),
 		day:date.getDay() || 7
 	};
+
 	var bbb = getDay() ;
-	var bbbday = (D.day - bbb%7)%7 || 7;
-	console.log(bbb,bbbday)
-	return bbbday ;
+	var bbbday = getMonthFirstDay(D.day,bbb) ;
+	return {
+		year:pointDayArr[0] ,
+		yearDates : (pointDayArr[0])%4==0?366:365 , // 一年总天数
+		month : pointDayArr[1] ,
+		monthDates : getMonthDates(pointDayArr[0],pointDayArr[1],pointDayArr[2]) , // 一月总天数
+		monthFirstDay : getMonthFirstDay(bbbday,(pointDayArr[2]-1)), // 一月第一天是星期几
+		date : pointDayArr[2] ,
+		dateDiffer : bbb , // 距今有多少天
+		day : bbbday ,
+		str : str // 时间字符串 2015-08-01 这种格式
+	}
+
+
+	function getMonthFirstDay(day,bbb){
+		// 获取当月1号星期几，所需参数，[当前星期几，与当前天数差]
+		var bbbday ;
+		if(bbb>0){
+			bbbday = (day - bbb%7)<0 ? ( day - bbb%7)%7+7  : (( day - bbb%7 )%7 || 7)
+		}else{
+			bbbday = ( day + Math.abs(bbb)%7)%7 || 7
+		}
+		return bbbday ;
+	}
+
+	function getMonthDates(year,month,date){
+		var arr31 = [1,3,5,7,8,10,12];
+		var arr30 = [4,6,9,11] ;
+		var arr28 = [2] ;
+		var days = 0 ;
+		arr31.forEach(function(ele){
+			if(ele==month) days = 31 ;
+		});
+		arr30.forEach(function(ele){
+			if(ele==month) days = 30 ;
+		});
+		arr28.forEach(function(ele){
+			if(ele==month)
+				days = year%4==0 ? 29 : 28 ;
+		});
+		return days ;
+	}
+
 	function getUtilDay(year,month,date){
 		var arr31 = [1,3,5,7,8,10,12];
 		var arr30 = [4,6,9,11] ;
@@ -77,61 +142,4 @@ function bitch(str){
 			day = getUtilDay(D.year,D.month,D.date) - getUtilDay(pointDayObj.year,pointDayObj.month,pointDayObj.date) ;
 		return day
 	}
-
 }
-
-
-var MyDate = (function(){
-	var d = new Date();
-	var month_31 = [1,3,5,7,8,10,12];
-	var month_30 = [4,6,9,11];
-	// 二月份根据2015年数不同，对应天数不同，能被四整除的年份为29天
-	console.log(d.getDay());
-	var year = d.getFullYear(); // 年份
-	var month = d.getMonth()+1 ; // 几月份
-	var day = d.getDay() || 7 ; // 星期几
-	var date = d.getDate() ; // 一月中的几天
-
-	function getMonthDate(month){
-		var monthDate ;
-		if(month_31.indexOf(month)>-1){
-			monthDate = 31 ;
-		}else if(month==2){
-			if(year%4==0){
-				monthDate = 29 ;
-			}else{
-				monthDate = 28 ;
-			}
-		}else{
-			monthDate = 30 ;
-		}
-		return monthDate ;
-	}
-
-	var monthDate = getMonthDate(8);
-	// 获取到monthDate后，就可以
-	function toList(monthDate){
-		var arr = [] ;
-		for( var i=1 ,len=monthDate+1; i<len ; i++){
-			arr.push('<span class="myDate-invable-item">'+i+'</span>') ;
-		}
-		return arr.join('') ;
-	}
-
-	console.log(month+'月有'+monthDate+'天');
-	document.querySelector('#myDate').innerHTML = ( toList(monthDate) );
-
-
-	// 有以上数据得出当月的天数，以及其对应星期几
-	console.log(year,month,date,day);
-
-	/*var obj = {
-		template:'<div> <div> <div>左</div> <div></div> <div>右</div> </div> <div> <div>1</div> <div>2</div> <div>3</div> <div>4</div> <div>5</div> <div>6</div> <div>7</div> </div> </div>',
-		render:function(){
-			var div = document.createElement('div');
-			div.innerHTML = this.template ;
-			document.body.appendChild(div);
-		}
-	};
-	obj.render();*/
-})();
