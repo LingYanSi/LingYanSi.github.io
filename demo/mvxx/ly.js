abaa097ee753f18d8bfbd4f202119adde3c12b12
灵岩 <1029891106@qq.com>
2015-08-24 08:40:24 +0800

    update

--- demo/mvxx/ly.js
+++ demo/mvxx/ly.js
@@ -2,7 +2,7 @@
 * @Author: 灵岩寺
 * @Date:   2015-08-22 17:51:26
 * @Last Modified by:   灵岩寺
-* @Last Modified time: 2015-08-23 18:14:04
+* @Last Modified time: 2015-08-24 08:35:21
 */
 
 'use strict';
@@ -13,10 +13,11 @@
  * 其次，还是分析需求吧，
 */
 
+// 对对象的监听
 function lyan(obj,property,fun){
     var dp = obj ;
     var value = dp[property] ;
-    Object.defineProperty(dp, 'name', {
+    Object.defineProperty(dp, property, {
         get:function(){
             // console.log('get获取值')
             return value ;
@@ -32,43 +33,46 @@ function lyan(obj,property,fun){
         configurable:true,
     });
 }
+
+// 对数组的监听
 function lyan_arr(obj,fun){
+    var Arr_proto = Array.prototype ;
+    var slice = [].slice ;
+    // push
     var pushValue = function(){
-        var arr = [].slice.call(arguments);
-        fun(arr);
-        return Array.prototype.push.apply(obj,arr);
+        var arr = slice.call(arguments);
+        fun.apply(null,arr);
+        return Arr_proto.push.apply(obj,arr);
     }
     Object.defineProperty(obj,'push',{
         get:function(){
             return pushValue ;
         },
-        set: function(newValue){
-
-        },
+        set: function(newValue){ },
         enumberable:true,
         configurable:true,
     });
 
+    // unshift
     var unshiftValue = function(){
-        var arr = [].slice.call(arguments);
-        fun(arr);
-        return Array.prototype.unshift.apply(obj,arr);
+        var arr = slice.call(arguments);
+        fun.apply(null,arr);
+        return Arr_proto.unshift.apply(obj,arr);
     }
     Object.defineProperty(obj,'unshift',{
         get:function(){
             return unshiftValue ;
         },
-        set: function(newValue){
-
-        },
+        set: function(newValue){ },
         enumberable:true,
         configurable:true,
     });
 
+    // splice 话说现如今终于彻底明白splice的用发了 --|
     var spliceValue = function(index,howmany){
-        var arr = [].slice.call(arguments);
-        fun(arr);
-        return Array.prototype.splice.apply(obj,arr);
+        var arr = slice.call(arguments);
+        fun.apply(null,arr);
+        return Arr_proto.splice.apply(obj,arr);
     }
     Object.defineProperty(obj,'splice',{
         get:function(){
@@ -80,6 +84,7 @@ function lyan_arr(obj,fun){
     });
 }
 
+// wtf
 function ly( obj ){
     var data = obj.data ,
         ele = obj.el ,
@@ -88,6 +93,7 @@ function ly( obj ){
     fenxi(template);
 }
 
+// 模版解析
 function fenxi(template){
     // template进来后，需要解析，绑定，简单一点的花就是匹配{{}}
     // 然后将花括号内的变量替换为字符串？
