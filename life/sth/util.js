392c677e3bcaf31b23e875cd58330e92d3eb4923
灵岩 <1029891106@qq.com>
2015-08-31 16:05:30 +0800

    update

--- life/sth/util.js
+++ life/sth/util.js
@@ -2,7 +2,7 @@
 * @Author: 灵岩寺
 * @Date:   2015-08-31 14:40:26
 * @Last Modified by:   灵岩寺
-* @Last Modified time: 2015-08-31 15:55:39
+* @Last Modified time: 2015-08-31 16:04:43
 */
 
 'use strict';
@@ -12,27 +12,27 @@ var data = [
         title:'局部',
         intro:'陈丹青主讲，喜欢这老头',
         url:'http://www.youku.com/show_page/id_zcc117696c7cb11e4b432.html',
-        logo:'',
+        logo:'http://lingyansi.github.io/images/icon/icon.jpg',
     },{
         title:'一千零一夜',
         intro:'梁文道，这份毅力是不错的',
         url:'http://www.youku.com/show_page/id_z0c9f6596c7cd11e4b2ad.html',
-        logo:'',
+        logo:'http://lingyansi.github.io/images/icon/icon.jpg',
     },{
         title:'逻辑思维',
         intro:'互联网经济，忽悠起',
         url:'http://www.youku.com/show_page/id_z5bdbf57c947311e3b8b7.html',
-        logo:'',
+        logo:'http://lingyansi.github.io/images/icon/luojisiwei.jpg',
     },{
         title:'太医来了',
         intro:'坐月子？大姨妈？中医？',
         url:'http://ipn.li/taiyilaile/',
-        logo:'',
+        logo:'http://lingyansi.github.io/images/icon/taiyi.jpg',
     },{
         title:'虹膜',
-        intro:'猪头massage',
-        url:'',
-        logo:'',
+        intro:'猪头magasa',
+        url:'http://www.zhihu.com/people/magasa',
+        logo:'http://lingyansi.github.io/images/icon/magasa.jpg',
     },{
         title:'葛巾',
         intro:'葛巾老师',
@@ -42,17 +42,17 @@ var data = [
         title:'视觉志',
         intro:'偶尔看看',
         url:'http://weibo.com/u/2141100877',
-        logo:'',
+        logo:'http://lingyansi.github.io/images/icon/shijuezhi.jpg',
     },{
         title:'新车评',
         intro:'偶尔看看',
         url:'',
-        logo:'',
+        logo:'http://lingyansi.github.io/images/icon/icon.jpg',
     },{
         title:'草榴社区',
         intro:'1024，www.t66y.com',
         url:'http://www.t66y.com',
-        logo:'',
+        logo:'http://lingyansi.github.io/images/icon/icon.jpg',
     },{
         title:'历史',
         intro:'我的知乎收藏夹',
