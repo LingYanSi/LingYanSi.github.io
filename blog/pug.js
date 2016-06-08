69a524f4018865413d0d0e9d5e55668c1f448cd9
wangyong <wangyong@qima-inc.com>
2016-06-08 17:39:36 +0800

    博客更新

--- blog/pug.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-var fs = require('fs')
-var pug = require('pug')
-var jadeTpl = fs.readFileSync( './jade/index.jade', 'utf8' )
-
-const tpl = pug.render(jadeTpl)
-console.log(tpl)
--- blog/push.sh
+++ blog/push.sh
@@ -1,13 +1,19 @@
 #/bin/bash!
 
+echo '--》复制js文件'
 # js文件
 rm -r ./js
 cp -r ./koa/static/js/ ./js/
 
+echo '--》复制database文件'
 # database文件
 rm -r ./database/
 cp -r ./koa/static/database/ ./database/
 
+echo '--》git提交文件'
 git add ../blog
 git commit -m '博客更新'
+
+echo '--》开始提交代码'
 git push origin master
+echo '--》代码提交完成'
