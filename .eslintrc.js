a97d676a28a4abc6944e16a160f2e4b948575f3c
wangyong <wangyong@qima-inc.com>
2016-06-08 17:51:45 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,3 +4,33 @@
 //         "react"
 //     ]
 // };
+
+// {
+//     "rules": {
+//         "quotes": [
+//             2,
+//             "single"
+//         ],
+//         "linebreak-style": [
+//             4,
+//             "unix"
+//         ],
+//         "semi": [
+//             2,
+//             "always"
+//         ]
+//     },
+//     "env": {
+//         "es6": true,
+//         "browser": true,
+//         "node": true
+//     },
+//     "extends": "eslint:recommended",
+//     "ecmaFeatures": {
+//         "jsx": true,
+//         "experimentalObjectRestSpread": true
+//     },
+//     "plugins": [
+//         "react"
+//     ]
+// }
