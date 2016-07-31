626422e7cc607c66eb3ead3c08d862b12299e83a
1029891106@qq.com <1029891106@qq.com>
2016-08-01 00:25:27 +0800

    slider优化， 支持动画等

--- funny/slider/index.js
+++ funny/slider/index.js
@@ -1,118 +1,135 @@
+'use strict'
 
-const NAME = 'mock'
-const Slider = function(dom, data = [], index = 1){
+const Slider = function(dom, data = [], index = 1, name = '') {
     this.current = index
     this.data = data
-    this.name = NAME
+    this.name = name
     this.LEN = data.length
     this.$ele = dom
+    this.$layer = dom.querySelector('.layer')
+    this.$content = dom.querySelector('.content')
+    this.cache = {}
+    this.animation = ''
 
-    // 初始化
+    // 动画结束，销毁class
+    this.$ele.addEventListener('animationend', () => {
+            this.animationend()
+        })
+        // 初始化
     this.go(index)
 }
 
 Slider.prototype = {
-    next(){
+    animationend() {
+        let classList = this.$content.classList
+        let type = this.animation
+        classList.remove(`${type}-init`)
+        classList.remove(`${type}-animation`)
+    },
+    next() {
         let index = this.current + 1
         this.go(index)
     },
-    prev(){
+    prev() {
         let index = this.current - 1
         this.go(index)
     },
-    go(index){
+    go(index) {
+        // 销毁之前所有动画
+        this.animationend()
+
         this.current = this.indexCheck(index, this.LEN)
+        let item = this.data[this.current - 1]
+        let type = this.animation = item.animation
+
+        // 如果有动画，就执行动画
+        // 位置初始化
+        type && this.$content.classList.add(`${type}-init`)
+            // 渲染内容
         this.render()
+
+        this.$layer.style.backgroundImage = `url(${item.bgd || ''})`
+            // 执行动画
+        type && this.$content.classList.add(`${type}-animation`)
+
+        // 保存页面状态到路由
+        Router.push(this.current)
     },
-    indexCheck(index, len){
+    indexCheck(index, len) {
         // debugger
-        if(index > len){
+        if (index > len) {
             return 1
         }
-        if(index < 1){
+        if (index < 1) {
             return len
         }
         return index
     },
-    getRawHTML(index, data){
+    getRawHTML(index, data) {
         let item = data[index]
         return new Promise(resolve => {
-            if(item.type == 'mark-down'){
+            if (item.type == 'mark-down') {
                 this.fetch(item.data)
                     .then(md => {
-                        console.log(md)
                         let html = converter.makeHtml(md)
                         html = `<div class="slider-item"><div style="display: inline-block;">${html}</div></div>`
                         resolve(html)
                     })
-            }else{
+            } else {
                 let html = `<div class="slider-item center">
-                    <h1>${item.data}</h1>
+                    <h1 style="font-size: 5rem;">${item.data}</h1>
                 </div>`
                 resolve(html)
             }
         })
 
     },
-    render(){
-        this.getRawHTML(this.current-1, this.data).then(html => {
-            this.$ele.innerHTML = html
+    render() {
+        this.getRawHTML(this.current - 1, this.data).then(html => {
+            this.$content.innerHTML = html
         })
     },
-    fetch(page){
+    fetch(page) {
         return new Promise(resolve => {
+            let cache = this.cache
             let url = `./${this.name}/${page}.md`
+            if (cache[url]) {
+                resolve(cache[url])
+                return
+            }
+            // let script = document.createElement('script')
+            // script.src = url
+            // script.type = 'text/template'
+            // script.onload = function(){
+            //     cache[url] = script.innerHTML
+            //     resolve(cache[url])
+            // }
+            // document.head.appendChild(script)
             fetch(url)
                 .then(res => res.text())
                 .then(data => {
-                    resolve(data)
+                    cache[url] = data
+                    resolve(cache[url])
                 })
         })
     }
 }
 
-var Data = [
-    {
-        type: 'html',
-        data: `Mock`,
-        animation: ''
-    },
-    {
-        type: 'mark-down',
-        data: 1,
-        animation: ''
-    },
-    {
-        type: 'mark-down',
-        data: 2,
-        animation: ''
-    },
-    {
-        type: 'mark-down',
-        data: 3,
-        animation: ''
-    },
-    {
-        type: 'html',
-        data: `Thanks`,
-        animation: ''
-    }
-]
-
-
 
 // 事件监听
-window.addEventListener('keyup', (event)=>{
+window.addEventListener('keyup', (event) => {
     // 39 -> 38 |^ 37 <- 40 |v
     const keyCode = event.keyCode
-    if(keyCode == 39 || keyCode == 40) {
+    if (keyCode == 39 || keyCode == 40) {
         fuck.next()
     }
-    if(keyCode == 38 || keyCode == 37) {
+    if (keyCode == 38 || keyCode == 37) {
         fuck.prev()
     }
 })
+
+// Markdown to html
 var converter = new showdown.Converter()
 
 // 获取数据
-let fuck = new Slider(document.querySelector('#app'), Data)
+let fuck = new Slider(document.querySelector('#app'), Data, Router.PAGE, Router.NAME)
--- funny/slider/mock/data.js
+++ funny/slider/mock/data.js
@@ -1,12 +1,29 @@
+
 var Data = [
     {
         type: 'html',
-        data: `FUCK U`,
-        animation: ''
+        data: `Mock`,
+        animation: 'down',
+        bgd: `http://s16.mogucdn.com/p1/160727/upload_ifrdandbmjstemzqmezdambqmeyde_715x530.jpg`
     },
     {
         type: 'mark-down',
         data: 1,
         animation: ''
+    },
+    {
+        type: 'mark-down',
+        data: 2,
+        animation: 'left'
+    },
+    {
+        type: 'mark-down',
+        data: 3,
+        animation: ''
+    },
+    {
+        type: 'html',
+        data: `Thanks`,
+        animation: 'big'
     }
 ]
