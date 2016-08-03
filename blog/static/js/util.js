1f5fadd1ea379201f046e8df36821d80a1d244eb
wangyong <1029891106@qq.com>
2016-08-03 11:35:26 +0800

    博客更新

---
+++ blog/static/js/util.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var Utils = {
+    // 关于时间
+    time: {
+        toString: function toString(time) {
+            var date = new Date(+time);
+            return date.getFullYear() + "-" + this.fillZero(date.getMonth() + 1) + "-" + this.fillZero(date.getDate()) + " " + this.fillZero(date.getHours()) + ":" + this.fillZero(date.getMinutes()) + ":" + this.fillZero(date.getSeconds());
+        },
+        fillZero: function fillZero(num) {
+            if (num < 10) {
+                return "0" + num;
+            }
+            return "" + num;
+        }
+    }
+};
