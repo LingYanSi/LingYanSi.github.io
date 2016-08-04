559cbff4f524bf2084c3e1a28062fa56426a0bff
wangyong <1029891106@qq.com>
2016-08-04 16:20:19 +0800

    copy——static

--- blog/push.sh
+++ blog/push.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 branch=`git rev-parse --abbrev-ref HEAD`
 
-echo '--》复制css文件'
+echo '--》复制static文件夹'
 # js文件
-rm -r ./static/css
-cp -r ./koa/static/css/ ./static/css/
+rm -r ./static/
+cp -r ./koa/static/ ./static/
 
-echo '--》复制js文件'
-# js文件
-rm -r ./static/js
-cp -r ./koa/static/js/ ./static/js/
-
-echo '--> 压缩js文件,打上md5'
+echo '--> 打上md5'
 gulp minify::app
 
 echo '--> 生成map'
@@ -21,9 +16,6 @@ echo '--》复制database文件'
 # database文件
 rm -r ./database/
 cp -r ./koa/static/database/ ./database/
-
-rm -r ./static/database/
-cp -r ./koa/static/database/ ./static/database/
 
 echo '-->渲染jade文件到index.html'
 node render_index.js
